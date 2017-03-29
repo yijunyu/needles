2180a6958e7d5420101f83358933f4f7bbf3409e
Yijun Yu <yijun.yu@gmail.com>
2017-03-29 22:16:25 +0100

    moved README.md location

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os.path, codecs, re
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = codecs.open(os.path.join(here, 'README.md'), encoding='utf8').read()
+CHANGES = codecs.open(os.path.join(here, 'CHANGES'), encoding='utf8').read()
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'needle', '__init__.py'),
+                 encoding='utf8') as version_file:
+    metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", version_file.read()))
+
+setup(name='needle',
+      version=metadata['version'],
+      description='Conversion between binary buffers and LSTM input sequences.',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        ],
+      author='Yijun Yu',
+      author_email='y.yu@open.ac.uk',
+      url='https://github.com/yijunyu/needles/',
+      keywords=['needle', 'packaging'],
+      license='GPL',
+      packages=[
+          'needle',
+          ],
+      extras_require={
+          ':python_version=="2.6"': ['argparse'],
+          'signatures': ['keyring', 'keyrings.alt'],
+          'signatures:sys_platform!="win32"': ['pyxdg'],
+          'signatures:python_version=="2.6"': ['importlib'],
+          'needleer-signatures': ['ed25519ll'],
+          'tool': []
+          },
+      tests_require=['jsonschema', 'pytest', 'coverage', 'pytest-cov'],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points = """\
+[console_scripts]
+needle = needle.needle:main
+
+[distutils.commands]
+bdist_needle = needle.bdist_needle:bdist_needle"""
+      )
+
